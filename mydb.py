4de748435f7398ae395fb6a75690200c4b13842f
Shahidavali <syedshahidavali@gmail.com>
2023-06-16 11:06:10 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,15 @@
+import mysql.connector
+dataBase=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='root123'
+    
+)
+
+# preapring the cursor object
+cursorObject=dataBase.cursor()
+
+#Create DataBase
+
+cursorObject.execute("CREATE DATABASE DCRM")
+print("ALl done re")
